e128c8d1da380cf8135def726d2716860a41da89
vertefra <verte.fra@gmail.com>
2020-11-07 07:22:59 -0500

    setup

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+import uvicorn
+
 
 app = FastAPI()
 
@@ -11,3 +13,7 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def test(request: Request):
     return templates.TemplateResponse('login', {"request": request})
+
+if __name__ === '__main__':
+    uvicorn.run()
+
